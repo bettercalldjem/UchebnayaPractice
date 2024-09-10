c6f94b75c10789fae574222702e09dee4d5e1e0d
bettercalldjem <165258598+bettercalldjem@users.noreply.github.com>
2024-09-10 10:56:44 +0700

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -681,3 +681,67 @@ class TimeCalculator
         Console.WriteLine($"Результат: {totalHours} часов {totalMinutes} минут");
     }
 }*/
+////27
+/*using System;
+class QuadraticEquationSolver
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите коэффициент a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите коэффициент b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите коэффициент c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        Solve(a, b, c);
+    }
+
+    static void Solve(double a, double b, double c)
+    {
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                {
+                    Console.WriteLine("Уравнение имеет бесконечно много решений");
+                }
+                else
+                {
+                    Console.WriteLine("Уравнение не имеет решений");
+                }
+            }
+            else
+            {
+                double root = -c / b;
+
+                Console.WriteLine($"Корень уравнения: x = {root}");
+            }
+        }
+        else
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+                Console.WriteLine($"Корни уравнения: x1 = {root1}, x2 = {root2}");
+            }
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+
+                Console.WriteLine($"Корень уравнения: x = {root}");
+            }
+            else
+            {
+                Console.WriteLine("Уравнение не имеет вещественных корней");
+            }
+        }
+    }
+}*/
